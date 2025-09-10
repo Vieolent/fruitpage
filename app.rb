889fbb5a2de831e70405764f3df079c0ed242512
Vieolent <miranda.l.bache@gmail.com>
2025-09-10 16:23:09 +0200

    did things

--- app.rb
+++ app.rb
@@ -1,1 +1,21 @@
 require 'sinatra'
+require 'slim'
+require 'sinatra/reloader'
+
+get('/') do
+ return "startsida"
+end
+
+get('/about') do
+  @shiny = 5
+  slim(:about)
+end
+
+get('/shady') do
+  slim(:shady)
+end
+
+get('/choose/:fruit') do
+  @fruit = params[:fruit]
+  slim(:printer)
+end
